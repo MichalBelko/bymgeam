31d3d00282920dd22ae85662ed3c4ffcd4e52b2f
Michal Belko <michal.belko@student.tuke.sk>
2025-05-10 12:58:52 +0200

    set initial page to login page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const isLoggedIn = localStorage.getItem("isLoggedIn");
-      if (isLoggedIn !== "true") {
-        router.replace("/login");
-      }
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn !== "true") {
+      router.replace("/login"); 
+    } else {
+      router.replace("/"); 
     }
   }, [router]);
 
